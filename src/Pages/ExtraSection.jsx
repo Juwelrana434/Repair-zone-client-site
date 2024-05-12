7cb07c45055fe800f9fd50ab94133e00769830e5
Juwelrana434 <jewel.rana434@gmail.com>
2024-05-12 08:26:12 +0600

    extra

---
+++ src/Pages/ExtraSection.jsx
@@ -0,0 +1,50 @@
+import image from "../../src/assets/images.jpg";
+import { GiAlarmClock } from "react-icons/gi";
+import { HiOutlineCurrencyBangladeshi } from "react-icons/hi";
+import { ImManWoman } from "react-icons/im";
+const ExtraSection = () => {
+  return (
+    <div className="m-4">
+      <div className="grid lg:md:grid-cols-2">
+        <div>
+          <img src={image} className="w-full h-full" />
+        </div>
+        <div className=" bg-[#eb4a62]">
+        <h1 className="text-white font-bold text-xl ml-10 mt-10">THE TECH REPAIR EXPERTS</h1>
+        <h1 className="text-white font-bold text-2xl ml-10 my-4">Why Choose Us?</h1>
+          <div>
+          <h1 className="text-center text-xl font-bold text-white">Fast Repairs</h1>
+            <div className="flex justify-start items-center my-4"><div className="mx-8"><GiAlarmClock className="bg-white text-[50px] text-[#eb4a62] rounded-full p-2" /></div>
+            
+            <h1 className="text-left text-white font-semibold">
+              In my role as a provider of fast repair services, I am dedicated
+              to delivering prompt, reliable, and high-quality solutions that
+              address client needs efficiently
+            </h1></div>
+            <h1 className="text-center text-xl font-bold text-white">Affordable Prices</h1>
+            <div className="flex justify-start items-center my-4"><div className="mx-8"><HiOutlineCurrencyBangladeshi className="bg-white text-[50px] text-[#eb4a62] rounded-full p-2" /></div>
+            
+            <h1 className="text-left text-white font-semibold">
+            Offering affordable prices as a service provider is crucial for
+              attracting and retaining customers while maintaining
+              competitiveness in the market. Here is how I approach pricing to
+              ensure affordability without compromising on quality or service
+            </h1></div>
+            <h1 className="text-center text-xl font-bold text-white">Qualified Staff</h1>
+            <div className="flex justify-start items-center mt-4 mb-8"><div className="mx-8"><ImManWoman className="bg-white text-[50px] text-[#eb4a62] rounded-full p-2" /></div>
+            
+            <h1 className="text-left text-white font-semibold">
+            Having a team of qualified staff is essential for delivering
+              reliable and high-quality services as a provider. Here is how I
+              ensure that my staff is well-trained, skilled, and capable of
+              meeting the needs of our customers
+            </h1></div>
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExtraSection;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import { useEffect} from "react";
 import Carousel from './../component/Carousel';
 import PopularServices from './PopularServices';
 import { Link } from "react-router-dom";
+import ExtraSection from './ExtraSection';
 const Home = () => {
   useEffect(() => {
     document.title = "Home";
@@ -13,10 +14,9 @@ const Home = () => {
     <div>
     <div className="mx-4"><Carousel></Carousel></div>
     <h1 className="text-center text-lime-900 text-3xl font-bold my-10"> Most Popular Services</h1>
-    <h1>Quality Service is Our Guarantee</h1>
     <PopularServices></PopularServices>
     <div className="mx-auto text-center"><Link to='/allServices'><button className="btn btn-primary">Show All</button></Link></div>
-      
+      <ExtraSection></ExtraSection>
     </div>
   );
 };
